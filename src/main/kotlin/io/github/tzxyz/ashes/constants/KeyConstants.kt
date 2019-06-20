d9347fc4d17d59b75e36ae79bbaef194fd8f404c
tzxyz <tz.zhuo@protonmail.com>
2019-06-20 10:04:47 +0800

    :sparkles: add ServerInfoView

--- src/main/kotlin/io/github/tzxyz/ashes/constants/KeyConstants.kt
+++ src/main/kotlin/io/github/tzxyz/ashes/constants/KeyConstants.kt
@@ -2,6 +2,6 @@ package io.github.tzxyz.ashes.constants
 
 class KeyConstants {
     companion object {
-        val KEY_CONSTANTS_SERVER_INFO = "KEY_CONSTANTS_SERVER_INFO"
+        const val KEY_CONSTANTS_SERVER_INFO = "KEY_CONSTANTS_SERVER_INFO"
     }
 }
--- src/main/kotlin/io/github/tzxyz/ashes/views/AshesCenterView.kt
+++ src/main/kotlin/io/github/tzxyz/ashes/views/AshesCenterView.kt
@@ -3,6 +3,7 @@ package io.github.tzxyz.ashes.views
 import com.jfoenix.controls.JFXRippler
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView
+import io.github.tzxyz.ashes.constants.KeyConstants
 import io.github.tzxyz.ashes.controllers.AshesKeyController
 import io.github.tzxyz.ashes.events.AshesOpenKeyViewEvent
 import io.github.tzxyz.ashes.models.*
@@ -21,54 +22,62 @@ class AshesCenterView : AshesBaseView() {
             tabMaxWidth = 100.0
             tabMinWidth = 100.0
             subscribe<AshesOpenKeyViewEvent> { e ->
-                val keyValue = keyController.getKeyAndValue(e.key)
-                val tab = tab(e.key) {
-                    graphic = JFXRippler(MaterialDesignIconView(MaterialDesignIcon.CODE_STRING, "1.4em"))
-                    contextMenu = contextmenu {
-                        item("Close").action {
-                            tabs.remove(selectionModel.selectedItem)
-                        }
-                        item("Close Left").action {
-                            val currentIndex = tabs.indexOf(selectionModel.selectedItem)
-                            val markedTabs = mutableListOf<Tab>()
-                            tabs.forEachIndexed { index, tab ->
-                                if (index < currentIndex) {
-                                    markedTabs.add(tab)
-                                }
-                            }
-                            tabs.removeAll(markedTabs)
-                        }
-                        item("Close Right").action {
-                            val currentIndex = tabs.indexOf(selectionModel.selectedItem)
-                            val markedTabs = mutableListOf<Tab>()
-                            tabs.forEachIndexed { index, tab ->
-                                if (index > currentIndex) {
-                                    markedTabs.add(tab)
-                                }
-                            }
-                            tabs.removeAll(markedTabs)
-                        }
-                        item("Close Other").action {
-                            val markedTabs = mutableListOf<Tab>()
-                            for (tab in tabs) {
-                                if (tab !== selectionModel.selectedItem)
-                                    markedTabs.add(tab)
-
-                            }
-                            tabs.removeAll(markedTabs)
-                        }
-                        item("Close All").action { tabs.removeAll { true } }
+                when(e.key) {
+                    KeyConstants.KEY_CONSTANTS_SERVER_INFO -> {
+                        val tab = tab("ServerInfo") {}
+                        selectionModel.select(tab)
                     }
-                    when(keyValue) {
-                        is AshesKeyStringValue -> add(find(AshesStringKeyView::class, Scope(), AshesStringKeyView::key to keyValue.key))
-                        is AshesKeyHashValue -> add(AshesHashKeyView(keyValue))
-                        is AshesKeyListValue -> add(AshesListKeyView(keyValue))
-                        is AshesKeySetValue -> add(AshesSetKeyView(keyValue))
-                        is AshesKeySortedSetValue -> add(AshesSortedSetKeyView(keyValue))
-                        else -> throw RuntimeException("Unknown Exception.")
+                    else -> {
+                        val keyValue = keyController.getKeyAndValue(e.key)
+                        val tab = tab(e.key) {
+                            graphic = JFXRippler(MaterialDesignIconView(MaterialDesignIcon.CODE_STRING, "1.4em"))
+                            contextMenu = contextmenu {
+                                item("Close").action {
+                                    tabs.remove(selectionModel.selectedItem)
+                                }
+                                item("Close Left").action {
+                                    val currentIndex = tabs.indexOf(selectionModel.selectedItem)
+                                    val markedTabs = mutableListOf<Tab>()
+                                    tabs.forEachIndexed { index, tab ->
+                                        if (index < currentIndex) {
+                                            markedTabs.add(tab)
+                                        }
+                                    }
+                                    tabs.removeAll(markedTabs)
+                                }
+                                item("Close Right").action {
+                                    val currentIndex = tabs.indexOf(selectionModel.selectedItem)
+                                    val markedTabs = mutableListOf<Tab>()
+                                    tabs.forEachIndexed { index, tab ->
+                                        if (index > currentIndex) {
+                                            markedTabs.add(tab)
+                                        }
+                                    }
+                                    tabs.removeAll(markedTabs)
+                                }
+                                item("Close Other").action {
+                                    val markedTabs = mutableListOf<Tab>()
+                                    for (tab in tabs) {
+                                        if (tab !== selectionModel.selectedItem)
+                                            markedTabs.add(tab)
+
+                                    }
+                                    tabs.removeAll(markedTabs)
+                                }
+                                item("Close All").action { tabs.removeAll { true } }
+                            }
+                            when(keyValue) {
+                                is AshesKeyStringValue -> add(find(AshesStringKeyView::class, Scope(), AshesStringKeyView::key to keyValue.key))
+                                is AshesKeyHashValue -> add(AshesHashKeyView(keyValue))
+                                is AshesKeyListValue -> add(AshesListKeyView(keyValue))
+                                is AshesKeySetValue -> add(AshesSetKeyView(keyValue))
+                                is AshesKeySortedSetValue -> add(AshesSortedSetKeyView(keyValue))
+                                else -> throw RuntimeException("Unknown Exception.")
+                            }
+                        }
+                        selectionModel.select(tab)
                     }
                 }
-                selectionModel.select(tab)
             }
         }
     }
